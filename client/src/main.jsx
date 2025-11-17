c32329d55b46f1764ae5cdff9271c7957cf4604c
ytijani <youssef.tijani@outlook.com>
2025-11-17 12:45:45 +0100

    fix: remove tenstack query debug icon

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,6 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ErrorBoundary } from "./components/ErrorBoundary";
 import { ToastProvider } from "./components/ui/toast";
 import { KeyboardShortcutsProvider } from "./components/KeyboardShortcuts";
@@ -29,7 +28,6 @@ createRoot(document.getElementById("root")).render(
         <ToastProvider>
           <QueryClientProvider client={queryClient}>
             <RouterProvider router={router} />
-            <ReactQueryDevtools initialIsOpen={false} />
           </QueryClientProvider>
         </ToastProvider>
       </KeyboardShortcutsProvider>
